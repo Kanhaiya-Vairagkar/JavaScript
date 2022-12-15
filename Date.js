a45397874c5c276441a7d7b987d49973bc5d1ecd
Kanhaiya-Vairagkar <kanhaiyavairagkar78@gmail.com>
2022-12-15 14:39:49 +0530

    AddNew

---
+++ Date.js
@@ -0,0 +1,12 @@
+// The Date object works with dates and times.
+// Date objects are created with new Date().
+// There are four ways of instantiating (creating) a date
+
+var r = new Date();
+console.log(r.getDate());
+console.log(r.getFullYear());
+console.log(r.getTime());
+console.log(r.getMilliseconds());
+console.log(r.getMinutes());
+console.log(r.getHours());
+console.log(r.getDay());
